6c11d4cc6f0fcba61b949972bd19edc11608fdae
Kiril <kiril2315@gmail.com>
2023-07-19 18:59:57 +0300

    users helper

--- tests/test_helper.js
+++ tests/test_helper.js
@@ -1,4 +1,5 @@
 const Blog = require( '../models/blog' )
+const User = require('../models/user')
 
 const initialBlogs = [
     { _id: "5a422a851b54a676234d17f7", title: "React patterns", author: "Michael Chan", url: "https://reactpatterns.com/", likes: 7, __v: 0 },
@@ -23,6 +24,15 @@ const blogsInDb = async () => {
   return blogs.map(blog => blog.toJSON())
 }
 
+const usersInDb = async () => {
+  const users = await User.find({})
+  return users.map(u => u.toJSON())
+}
+
+
 module.exports = {
-  initialBlogs, nonExistingId, blogsInDb
+  initialBlogs, 
+  nonExistingId, 
+  blogsInDb,
+  usersInDb,
 }
