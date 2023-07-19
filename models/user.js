83d754c5dabfb8f8a85edc8ef8a683a6a6adee52
Kiril <kiril2315@gmail.com>
2023-07-19 19:12:27 +0300

    users get

--- models/user.js
+++ models/user.js
@@ -1,7 +1,12 @@
-const mongoose = require('mongoose')
+const mongoose = require( 'mongoose' )
+const uniqueValidator = require( 'mongoose-unique-validator' )
 
-const userSchema = new mongoose.Schema({
-  username: String,
+const userSchema = new mongoose.Schema( {
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  },
   name: String,
   passwordHash: String,
   notes: [
@@ -10,18 +15,21 @@ const userSchema = new mongoose.Schema({
       ref: 'Blog'
     }
   ],
-})
+} )
 
-userSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
+userSchema.plugin(uniqueValidator)
+
+userSchema.set( 'toJSON', {
+  transform: ( document, returnedObject ) =>
+  {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
     // the passwordHash should not be revealed
     delete returnedObject.passwordHash
   }
-})
+} )
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model( 'User', userSchema )
 
 module.exports = User
