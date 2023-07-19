640cc9554ea43753f5a6a80b8d9e1d01be7e89f3
Kiril <kiril2315@gmail.com>
2023-07-19 14:19:00 +0300

    async/await

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,26 +1,18 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get( '/', ( request, response ) =>
+blogsRouter.get( '/', async ( request, response ) =>
 {
-    Blog
-        .find( {} )
-        .then( blogs =>
-        {
-            response.status( 200 ).json( blogs )
-        } )
+    let blogs = await Blog.find( {} )
+    response.status( 200 ).json( blogs )
 } )
 
-blogsRouter.post( '/', ( request, response ) =>
+blogsRouter.post( '/', async ( request, response ) =>
 {
     const blog = new Blog( request.body )
 
-    blog
-        .save()
-        .then( result =>
-        {
-            response.status( 201 ).json( result )
-        } )
+    let result = await blog.save()
+    response.status( 201 ).json( result )
 } )
 
 module.exports = blogsRouter
