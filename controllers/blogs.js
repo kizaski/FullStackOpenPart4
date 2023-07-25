9b0d460e9e687835a909e14878097350cf49f096
Kiril <kiril2315@gmail.com>
2023-07-25 19:35:36 +0300

    blogs populate

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,10 +1,10 @@
 const blogsRouter = require( 'express' ).Router()
 const Blog = require( '../models/blog' )
-const User = require('../models/user')
+const User = require( '../models/user' )
 
 blogsRouter.get( '/', async ( request, response ) =>
 {
-    let blogs = await Blog.find( {} )
+    let blogs = await Blog.find( {} ).populate( 'user', { username: 1, name: 1 } )
     response.status( 200 ).json( blogs )
 } )
 
