83220945a3aa576ac95f842d81e49895780da26d
Kiril <kiril2315@gmail.com>
2023-07-25 17:57:31 +0300

    oopsname

--- tests/list_helper.test.js
+++ tests/list_helper.test.js
@@ -2,7 +2,7 @@ const listHelper = require( '../utils/list_helper' )
 
 describe( 'total likes', () =>
 {
-    test( 'when list has only one blog, equals the likes of that', () =>
+    test( 'of multiple blogs', () =>
     {
         const blogs = [
             { _id: "5a422a851b54a676234d17f7", title: "React patterns", author: "Michael Chan", url: "https://reactpatterns.com/", likes: 7, __v: 0 },
