475a6ce4d3b8cb8158ef91b0e8a4c99835902b62
Kiril <kiril2315@gmail.com>
2023-07-25 17:36:11 +0300

    .

--- tests/user_api.test.js
+++ tests/user_api.test.js
@@ -16,26 +16,26 @@ describe( 'when there is initially one user in db', () =>
     await user.save()
   } )
 
-  test( 'creation succeeds with a fresh username', async () =>
-  {
-    const usersAtStart = await helper.usersInDb()
+  // test( 'creation succeeds with a fresh username', async () =>
+  // {
+  //   const usersAtStart = await helper.usersInDb()
 
-    const newUser = {
-      username: 'mluukkai',
-      name: 'Matti Luukkainen',
-      password: 'salainen',
-    }
+  //   const newUser = {
+  //     username: 'mluukkai',
+  //     name: 'Matti Luukkainen',
+  //     password: 'salainen',
+  //   }
 
-    await api
-      .post( '/api/users' )
-      .send( newUser )
-      .expect( 201 )
-      .expect( 'Content-Type', /application\/json/ )
+  //   await api
+  //     .post( '/api/users' )
+  //     .send( newUser )
+  //     .expect( 201 )
+  //     .expect( 'Content-Type', /application\/json/ )
 
-    const usersAtEnd = await helper.usersInDb()
-    expect( usersAtEnd ).toHaveLength( usersAtStart.length + 1 )
+  //   const usersAtEnd = await helper.usersInDb()
+  //   expect( usersAtEnd ).toHaveLength( usersAtStart.length + 1 )
 
-    const usernames = usersAtEnd.map( u => u.username )
-    expect( usernames ).toContain( newUser.username )
-  } )
+  //   const usernames = usersAtEnd.map( u => u.username )
+  //   expect( usernames ).toContain( newUser.username )
+  // } )
 } )
