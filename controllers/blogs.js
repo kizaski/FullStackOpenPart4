80a845023dba1ed8cffecf47f5ab9cc99d5e90f3
Kiril <kiril2315@gmail.com>
2023-07-26 20:13:33 +0300

    token extractor move to middleware

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -4,14 +4,13 @@ const User = require( '../models/user' )
 
 const jwt = require('jsonwebtoken')
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.startsWith('Bearer ')) {
-    return authorization.replace('Bearer ', '')
-  }
-  return null
-}
-
+// const getTokenFrom = request => {
+//   const authorization = request.get('authorization')
+//   if (authorization && authorization.startsWith('Bearer ')) {
+//     return authorization.replace('Bearer ', '')
+//   }
+//   return null
+// }
 
 blogsRouter.get( '/', async ( request, response ) =>
 {
@@ -23,7 +22,7 @@ blogsRouter.post( '/', async ( request, response ) =>
 {
     // const user = await User.findById(request.body.userId)
 
-    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
     if (!decodedToken.id) {
       return response.status(401).json({ error: 'token invalid' })
     }
