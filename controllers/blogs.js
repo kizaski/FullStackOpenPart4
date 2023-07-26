72aea6b20f095ca15025ad8aa5aed686da6a65d1
Kiril <kiril2315@gmail.com>
2023-07-26 18:12:12 +0300

    TOKENS!!!!

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -2,6 +2,17 @@ const blogsRouter = require( 'express' ).Router()
 const Blog = require( '../models/blog' )
 const User = require( '../models/user' )
 
+const jwt = require('jsonwebtoken')
+
+const getTokenFrom = request => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    return authorization.replace('Bearer ', '')
+  }
+  return null
+}
+
+
 blogsRouter.get( '/', async ( request, response ) =>
 {
     let blogs = await Blog.find( {} ).populate( 'user', { username: 1, name: 1 } )
@@ -11,6 +22,12 @@ blogsRouter.get( '/', async ( request, response ) =>
 blogsRouter.post( '/', async ( request, response ) =>
 {
     // const user = await User.findById(request.body.userId)
+
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+      return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
 
     const blog = new Blog( request.body )
 
