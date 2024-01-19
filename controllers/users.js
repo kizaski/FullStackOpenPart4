9342f9abb903d0af20bb55edcd7401dc39eac91a
Kiril <kiril2315@gmail.com>
2024-01-19 14:26:17 +0200

    pass lenght check

--- controllers/users.js
+++ controllers/users.js
@@ -18,6 +18,11 @@ usersRouter.post( '/', async ( request, response ) =>
 {
   const { username, name, password } = request.body
 
+  if ( password.length < 3 )
+  {
+    return response.status( 400 ).json( { error: 'Password must be at least 3 characters long.' } )
+  }
+
   const saltRounds = 10
   const passwordHash = await bcrypt.hash( password, saltRounds )
 
