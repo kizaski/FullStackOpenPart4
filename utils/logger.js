3bfd18df80a386736c8cea6d54a03b2ce24345c5
Kiril <kiril2315@gmail.com>
2024-03-10 20:46:12 +0200

    log on NODE_ENV=test

--- utils/logger.js
+++ utils/logger.js
@@ -1,6 +1,6 @@
 const info = ( ...params ) =>
 {
-    if ( process.env.NODE_ENV !== 'test' )
+    //if ( process.env.NODE_ENV !== 'test' )
     {
         console.log( ...params )
     }
@@ -8,7 +8,7 @@ const info = ( ...params ) =>
 
 const error = ( ...params ) =>
 {
-    if ( process.env.NODE_ENV !== 'test' )
+    //if ( process.env.NODE_ENV !== 'test' )
     {
         console.error( ...params )
     }
